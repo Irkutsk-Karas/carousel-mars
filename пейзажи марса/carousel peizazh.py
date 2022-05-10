5947e581d75ef16526bf6f347d2cb0d31c7bc141
Daria Karas <94908174+Irkutsk-Karas@users.noreply.github.com>
2022-05-10 16:42:12 +0800

    Update carousel peizazh.py

--- пейзажи марса/carousel peizazh.py
+++ пейзажи марса/carousel peizazh.py
@@ -4,42 +4,6 @@ from PIL import Image
 app = Flask(__name__)
 
 
-@app.route('/load_photo', methods=['POST', 'GET'])
-def load_photo():
-    if request.method == 'POST':
-        f = request.files['file']
-        f.save('static/photos/download.png')
-        # with Image.open('static/photos/download.png', 'rgb') as d:
-    return f"""<!doctype html>
-                <html lang="en">
-                  <head>
-                    <meta charset="utf-8">
-                    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-                    <link rel="stylesheet" 
-                    href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" 
-                    integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" 
-                    crossorigin="anonymous">
-                    <link rel="stylesheet" 
-                        href={url_for('static', filename='css/formstyle.css')}>
-                    <title>Отбор астронавтов</title>
-                  </head>
-                  <body>
-                        <h1 align="center">Загрузка фотографии</h1>
-                        <h2 align="center">для участия в миссии</h2>
-                            <div>
-                                <form class="login_form" method="post" enctype="multipart/form-data">
-                                    <div class="form-group">
-                                        <label for="photo">Приложите фотографию</label>
-                                        <input type="file" class="form-control-file" id="photo" name="file">
-                                    </div>
-                                    <img src={url_for('static', filename='photos/download.png')} width=400>
-                                    <button type="submit" class="btn btn-primary">Отправить</button>
-                                </form>
-                            </div>
-                  </body>
-                </html>"""
-
-
 @app.route('/carousel')
 def carousel():
     return f'''<!doctype html>
